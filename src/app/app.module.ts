fbc5cb7b7eebeec480f94e187df8ead93d04abe4
denn95 <deniron13@gmail.com>
2020-04-22 14:56:42 -0600

    Diseño para registro de activo fijo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routing, appRoutingProviders } from './app.routing';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
+import { AgregarComponent } from './components/activoFijo/agregar/agregar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SidemenuComponent
+    SidemenuComponent,
+    AgregarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,3 +1,19 @@
 //importar los modulos del router de angular
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+//Importar componentes a los cuales les quiero hacer una paina exclusiva
+import { AgregarComponent } from './components/activoFijo/agregar/agregar.component';
+
+
+
+
+// Array de rutas
+const appRoutes: Routes = [
+    {path: 'agregar', component: AgregarComponent}
+   
+];
+
+// exportar el modulo de rutas
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
