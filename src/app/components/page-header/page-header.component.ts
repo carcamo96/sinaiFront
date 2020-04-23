bd101212e88bcc100324a30049f46ca0f3b4cebd
Alex Carcamo <walter_alx@hotmail.com>
2020-04-23 17:45:18 -0600

    Inicio del formulario de nuevo paciente

--- src/app/components/page-header/page-header.component.ts
+++ src/app/components/page-header/page-header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PageHeaderComponent implements OnInit {
 
   @Input() titulo: string;
+  @Input() icono: string;
   @Input() breadcrumbs: any[];
 
 
