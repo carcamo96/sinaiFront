a876e265f1779e982903bb750521f5efcd4ace9c
Alex Carcamo <walter_alx@hotmail.com>
2020-05-02 21:56:27 -0600

    terminando de arreglar fechaNac y edad

--- src/app/pages/formulario-paciente/formulario-paciente.component.ts
+++ src/app/pages/formulario-paciente/formulario-paciente.component.ts
@@ -4,6 +4,7 @@ import { Paciente } from '../../models/paciente';
 //Importando la clase para manejar las alertas toastr para angular
 import { ToastrService } from 'ngx-toastr';
 
+
 @Component({
   selector: 'app-formulario-paciente',
   templateUrl: './formulario-paciente.component.html',
@@ -19,7 +20,8 @@ export class FormularioPacienteComponent implements OnInit {
   public paciente: any;
   public form: NgForm;
   public edad: number;
-
+  
+  public fechaAux = '';
 
   constructor(private toastr: ToastrService) {
     this.paciente = {
@@ -35,27 +37,67 @@ export class FormularioPacienteComponent implements OnInit {
       direccion: '',
       otrosDatos: ''
     }
+
+    this.inicializarFechaActual();
+
   }
 
   ngOnInit(): void {
-    
+      
   }
 
+  //Enviando datos
   onSubmit(f:NgForm){
-      //console.log(this.paciente);
+  
       console.log(f.value);
       this.showSuccess("Se ha registrado un nuevo paciente", "Nuevo expediente");
+      f.resetForm({r1: 'M' }); //importante!!! resetea y se le pasa un json con los nuevo valores por defecto
+      this.edad = undefined;
+      this.limpiarCampos(); //limpia los campos del objeto auxiliar
+
   }
 
+  //Inicializar fecha actual en el date picker
+  inicializarFechaActual(){
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth()+1; //January is 0!
+      var yyyy = today.getFullYear();
+      if(dd<10){
+              var dia=dd.toString();
+              dia='0'+dd;
+          } 
+          if(mm<10){
+            var mes = mm.toString();
+              mes='0'+mm;
+          } 
+
+      this.fechaAux = yyyy+'-'+mes+'-'+dia;
+  }
+
+
+  // Evento de cambio de fecha para mostrar la edad
   cambioFecha(event){
      
       let anioActual = new Date();
+      let mesActual = anioActual.getMonth() + 1;
+      let diaActual = anioActual.getDate();
+
       let fechaNac = new Date(event);
+      let mesNac = fechaNac.getMonth() + 1;
+      let diaNac = fechaNac.getDate();
 
-      this.paciente.fechaNac = event;
+      this.paciente.fechaNac = event; // guardando en el objeto auxiliar
 
-      //Arreglar calculo con fecha completa con moment js
+      //Calculando edad relativa
       let edad = anioActual.getFullYear() - fechaNac.getFullYear();
+
+      if(mesActual < mesNac){
+          edad--;
+      }
+      if((mesNac == mesActual) && (diaActual < diaNac)){
+          edad--;
+      }
 
       this.edad = edad;
 
