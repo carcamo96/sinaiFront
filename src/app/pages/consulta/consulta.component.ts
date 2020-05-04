59aa5661e1de4cf157a58d2e38274442ea72cad8
denn95 <deniron13@gmail.com>
2020-05-03 22:56:16 -0600

    Integrando barra de carga a formulario de Consulta

--- src/app/pages/consulta/consulta.component.ts
+++ src/app/pages/consulta/consulta.component.ts
@@ -4,6 +4,8 @@ import {NgForm} from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { ConsultaService } from '../../services/consulta.service';
 import { Consulta } from 'src/app/models/consulta';
+import { LoadingBarService } from '@ngx-loading-bar/core';
+import { Paciente } from '../../models/paciente';
 
 
 @Component({
@@ -19,22 +21,24 @@ public breads: any[] = [
   {titulo: 'Home', link: '/home'}
 ];
 
-public consulta: Consulta;
+
 public nomPaciente: string;
 public atl = new Date();
 public status: string;
-public consultas: any;
+public consulta: any;
+public spinnStatus: boolean;
+
+
 
   constructor(
     private toastr: ToastrService,
-    private _consultaService: ConsultaService
+    private _consultaService: ConsultaService,
+    private loadingBarService: LoadingBarService
     ) { 
-    this.nomPaciente = "";
-    this.consulta = new Consulta('','','','','','','','','','','','','','',);
-    this.consulta.paciente='5eade4cf9ec9ee0017a5f982';
-    this.consulta.fechaCre='02/06/2010';
-    this.consulta.indiceMC='14';
-    this.consultas = {
+    this.consulta = {
+      paciente:'5eac959e35217825fdb963d8',
+      fechaCre:'02/06/2010',
+      indiceMC:'14',
       motivo: '',
       tiemSintoma: '',
       fechaConsul: '',
@@ -45,9 +49,9 @@ public consultas: any;
       talla:'',
       temperatura:'',
       presionArt:'',
-      freCardia:'',
-      indiceMC:''
+      freCardia:''
     }
+
     this.consulta.fechaConsul=this.atl.toLocaleDateString() +' '+ this.atl.toLocaleTimeString();
   }
 
@@ -62,35 +66,83 @@ public consultas: any;
     );
   }
 
-  onSubmit(){
-    //console.log(f.value);
-    //this.showSuccess("Se ha realizado una nueva consulta", "Nueva consulta");
-    
-
-    this._consultaService.create(this.consulta).subscribe(
-      response => {
-        if (response.status == 'succes') {
-          this.status = 'succes';
-          this.consulta = response.consultas;
-
-          //Alert
-          this.showSuccess("Se ha realizado una nueva consulta", "Nueva consulta");
-          
-        }else{
-          this.status = 'error';
+  onSubmit(f:NgForm){
+    this.loadingBarService.start();
+    if(f.valid)
+    { 
+      let consulta = new Consulta(
+        this.consulta.paciente,
+        this.consulta.motivo,
+        this.consulta.tiemSintoma,
+        this.consulta.fechaConsul,
+        this.consulta.historia,
+        this.consulta.antePatol,
+        this.consulta.alergias,
+        this.consulta.peso,
+        this.consulta.talla,
+        this.consulta.temperatura,
+        this.consulta.presionArt,
+        this.consulta.freCardia,
+        this.consulta.indiceMC,
+        this.consulta.fechaCre
+      );
+      this._consultaService.create(consulta).subscribe(
+        response => {
+          if (response.status == 'success') {
+            this.loadingBarService.complete();
+            this.status = 'success';
+            this.spinnStatus=false;
+            //Alert
+            this.showSuccess("Se ha realizado una nueva consulta", "Nueva consulta");
+            
+          }else{
+            this.status = 'error';
+          }
+        },
+        error => {
+            console.log(error);
+            this.loadingBarService.complete();
+            this.status = 'error';
+            this.showError("No se ha conctado con el servidor", "Error");
         }
-      },
-      error => {
-          console.log(error);
-          this.status = 'error';
-      }
 
-    );
+      );
+   }
+   f.resetForm();
+   this.limpiarCampos();
+}
+
+spnChange(){
+  this.spinnStatus = true;
 }
 
   // Alerta de exito
   showSuccess(mensaje: string, titulo: string) {
     this.toastr.success(mensaje, titulo);
+  }
+
+  limpiarCampos(){
+    this.consulta = {
+      motivo: '',
+      tiemSintoma: '',
+      fechaConsul: '',
+      historia: '',
+      antePatol: '',
+      alergias: '',
+      peso:'',
+      talla:'',
+      temperatura:'',
+      presionArt:'',
+      freCardia:'',
+      indiceMC:''
+    }
+  }
+
+  showError(mensaje: string, titulo: string) {
+    this.toastr.error(mensaje, titulo, {
+        progressBar: true,
+        progressAnimation: 'decreasing'
+    });
   }
 
 }
