58701764418c869e5b4188e57800fc0852b87d33
denn95 <deniron13@gmail.com>
2020-05-30 23:04:57 -0600

    Ayuda en consulta

--- src/app/pages/consulta/consulta.component.ts
+++ src/app/pages/consulta/consulta.component.ts
@@ -1,46 +1,41 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 //Importando la clase para manejar las alertas toastr para angular
-import { ToastrService } from 'ngx-toastr';
-import { ConsultaService } from '../../services/consulta.service';
-import { Consulta } from 'src/app/models/consulta';
-import { LoadingBarService } from '@ngx-loading-bar/core';
-import { Paciente } from '../../models/paciente';
-import { ActivatedRoute, Router } from '@angular/router';
-import { PacienteService } from '../../services/paciente.service';
-import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
-
+import { ToastrService } from "ngx-toastr";
+import { ConsultaService } from "../../services/consulta.service";
+import { Consulta } from "src/app/models/consulta";
+import { LoadingBarService } from "@ngx-loading-bar/core";
+import { Paciente } from "../../models/paciente";
+import { ActivatedRoute, Router } from "@angular/router";
+import { PacienteService } from "../../services/paciente.service";
+import { SwalComponent } from "@sweetalert2/ngx-sweetalert2";
 
 @Component({
-  selector: 'app-consulta',
-  templateUrl: './consulta.component.html',
-  styleUrls: ['./consulta.component.css'],
-  providers: [ConsultaService, PacienteService]
+  selector: "app-consulta",
+  templateUrl: "./consulta.component.html",
+  styleUrls: ["./consulta.component.css"],
+  providers: [ConsultaService, PacienteService],
 })
 export class ConsultaComponent implements OnInit {
+  @ViewChild("redireccionSwal") private redireccionSwal: SwalComponent;
+  @ViewChild("errorSwal") private errorSwal: SwalComponent;
 
-  @ViewChild('redireccionSwal') private redireccionSwal: SwalComponent;
-  @ViewChild('errorSwal') private errorSwal: SwalComponent;
+  //Se le pasan los titulos y los links de las paginas que preseden esta pagina
+  public breads: any[] = [{ titulo: "Home", link: "/home" }];
 
-//Se le pasan los titulos y los links de las paginas que preseden esta pagina
-public breads: any[] = [
-  {titulo: 'Home', link: '/home'}
-];
+  public nomPaciente: string;
+  public atl = Date.now();
+  public status: string;
+  public consulta: any;
+  public spinnStatus: boolean;
+  public paciente: Paciente;
+  public indice: string;
+  public idpa: string;
+  public edad: number;
+  public fechaAux = "";
+  public ayuda = false;
 
-
-public nomPaciente: string;
-public atl =Date.now();
-public status: string;
-public consulta: any;
-public spinnStatus: boolean;
-public paciente: Paciente;
-public indice: string;
-public idpa:string;
-public edad: number;
-public fechaAux="";
-
-progress = 0;
-
+  progress = 0;
 
   constructor(
     private toastr: ToastrService,
@@ -49,44 +44,41 @@ progress = 0;
     private loadingBarService: LoadingBarService,
     private _route: ActivatedRoute,
     private _router: Router
-    ) { 
-      
+  ) {
     this.consulta = {
-      paciente: '',
-      fechaCre:'02/06/2010',
-      indiceMC:'',
-      motivo: '',
-      tiemSintoma: '',
-      fechaConsul: '',
-      historia: '',
-      antePatol: '',
-      alergias: '',
-      peso:'',
-      talla:'',
-      temperatura:'',
-      presionArt:'',
-      freCardia:'',
-      diagnostico:''
-    }
-    this.consulta.fechaConsul=this.inicializarFechaConsulP(this.atl);
+      paciente: "",
+      fechaCre: "02/06/2010",
+      indiceMC: "",
+      motivo: "",
+      tiemSintoma: "",
+      fechaConsul: "",
+      historia: "",
+      antePatol: "",
+      alergias: "",
+      peso: "",
+      talla: "",
+      temperatura: "",
+      presionArt: "",
+      freCardia: "",
+      diagnostico: "",
+    };
+    this.consulta.fechaConsul = this.inicializarFechaConsulP(this.atl);
     //this.consulta.fechaConsul= new Date(this.consulta.fechaConsul);
-    
+
     this.inicializarFechaActual();
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.cargarPaciente();
-    
+
     console.log(this.consulta.fechaConsul);
   }
 
-  onSubmit(f:NgForm){
-    
+  onSubmit(f: NgForm) {
     this.loadingBarService.start();
     this.progress = 30;
 
-    if(f.valid)
-    { 
+    if (f.valid) {
       let consulta = new Consulta(
         this.idpa,
         this.consulta.motivo,
@@ -107,42 +99,39 @@ progress = 0;
       this.progress = 50;
       console.log(consulta);
       this._consultaService.create(consulta).subscribe(
-        response => {
-
+        (response) => {
           this.progress = 100;
           //console.log(this.consulta=response.consulta);
-          if (response.status == 'success') {
+          if (response.status == "success") {
             this.loadingBarService.complete();
-            this.status = 'success';
-            this.consulta=response.consulta;
+            this.status = "success";
+            this.consulta = response.consulta;
 
             //Alert
             this.redireccionSwal.fire();
-            
-          }else{
+          } else {
             this.loadingBarService.complete();
-            console.log('else');
-            this.status = 'error';
+            console.log("else");
+            this.status = "error";
             this.errorSwal.fire();
           }
         },
-        error => {
-          console.log('error');
-            console.log(error);
-            this.loadingBarService.complete();
-            this.status = 'error';
-            this.errorSwal.fire();
+        (error) => {
+          console.log("error");
+          console.log(error);
+          this.loadingBarService.complete();
+          this.status = "error";
+          this.errorSwal.fire();
         }
-
       );
-   }
-   f.resetForm();
-   this.limpiarCampos();
-}
+    }
+    f.resetForm();
+    this.limpiarCampos();
+  }
 
-spnChange(){
-  this.spinnStatus = true;
-}
+  spnChange() {
+    this.spinnStatus = true;
+  }
 
   // Alerta de exito
   showSuccess(mensaje: string, titulo: string) {
@@ -150,96 +139,96 @@ spnChange(){
   }
 
   //Inicializar fecha actual en el date picker
-  inicializarFechaActual(){
+  inicializarFechaActual() {
     var today = new Date();
     var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
+    var mm = today.getMonth() + 1; //January is 0!
     var yyyy = today.getFullYear();
-    if(dd<10){
-            var dia=dd.toString();
-            dia='0'+dd;
-        } 
-        if(mm<10){
-          var mes = mm.toString();
-            mes='0'+mm;
-        } 
+    if (dd < 10) {
+      var dia = dd.toString();
+      dia = "0" + dd;
+    }
+    if (mm < 10) {
+      var mes = mm.toString();
+      mes = "0" + mm;
+    }
 
-    this.fechaAux = yyyy+'-'+mes+'-'+dia;
+    this.fechaAux = yyyy + "-" + mes + "-" + dia;
   }
 
-  limpiarCampos(){
+  limpiarCampos() {
     this.consulta = {
-      motivo: '',
-      tiemSintoma: '',
-      fechaConsul: '',
-      historia: '',
-      antePatol: '',
-      alergias: '',
-      peso:'',
-      talla:'',
-      temperatura:'',
-      presionArt:'',
-      freCardia:'',
-      indiceMC:''
-    }
+      motivo: "",
+      tiemSintoma: "",
+      fechaConsul: "",
+      historia: "",
+      antePatol: "",
+      alergias: "",
+      peso: "",
+      talla: "",
+      temperatura: "",
+      presionArt: "",
+      freCardia: "",
+      indiceMC: "",
+    };
   }
 
   showError(mensaje: string, titulo: string) {
     this.toastr.error(mensaje, titulo, {
-        progressBar: true,
-        progressAnimation: 'decreasing'
+      progressBar: true,
+      progressAnimation: "decreasing",
     });
   }
 
-  cargarPaciente(){
-    this._route.params.subscribe(params => {
-      this.idpa = params['id'];
+  cargarPaciente() {
+    this._route.params.subscribe((params) => {
+      this.idpa = params["id"];
 
       this._pacienteService.getPaciente(this.idpa).subscribe(
-        response => {
+        (response) => {
           console.log(response);
           this.paciente = response.paciente;
-          this.edad = this.obtenerEdad(new Date(), new Date(this.paciente.fechaNac));
-          this.paciente.nombre = response.paciente.nombre+' '+response.paciente.apellidos;
+          this.edad = this.obtenerEdad(
+            new Date(),
+            new Date(this.paciente.fechaNac)
+          );
+          this.paciente.nombre =
+            response.paciente.nombre + " " + response.paciente.apellidos;
         },
-        error => {
-          console.log("nadaaa "+error);
+        (error) => {
+          console.log("nadaaa " + error);
           //this._router.navigate(['/home']);
         }
       );
     });
-    
   }
 
-  cal(event){
-
-    var pesoP=event;
+  cal(event) {
+    var pesoP = event;
 
     this.indice = pesoP;
   }
 
-  cal2(event){
-
-    var tallaP=event;
+  cal2(event) {
+    var tallaP = event;
     var pes = +this.indice;
     let indice;
-    if (tallaP != null || this.consulta.talla !=null) {
-       indice = pes / Math.pow(tallaP,2);
-       this.indice =''+ indice;
-    }else{
-      this.indice=''+0;
+    if (tallaP != null || this.consulta.talla != null) {
+      indice = pes / Math.pow(tallaP, 2);
+      this.indice = "" + indice;
+    } else {
+      this.indice = "" + 0;
     }
-    
   }
 
-  cal3(){
-    var pes =  parseFloat(this.consulta.peso);
-    var tall =  parseFloat(this.consulta.talla);
-    if(tall != 0 || tall != null || tall != Infinity){
-      let indice = pes / Math.pow(tall,2);
-      this.indice =''+ parseFloat(''+indice).toFixed(1);
+  cal3() {
+    var pes = parseFloat(this.consulta.peso);
+    var tall = parseFloat(this.consulta.talla);
+    if (tall != 0 || tall != null || tall != Infinity) {
+      let indice = pes / Math.pow(tall, 2);
+      this.indice = "" + parseFloat("" + indice).toFixed(1);
       this.consulta.indiceMC = this.indice;
-    }else{
+    } else {
       //this.indice = '';
     }
   }
@@ -263,8 +252,7 @@ spnChange(){
     return yyyy + "-" + mes + "-" + dia;
   }
 
-  obtenerEdad(fechaActual, fechaNac){
-
+  obtenerEdad(fechaActual, fechaNac) {
     let diaActual = fechaActual.getDate();
     let mesActual = fechaActual.getMonth() + 1;
 
@@ -274,23 +262,39 @@ spnChange(){
     //Calculando edad relativa
     let edad = fechaActual.getFullYear() - fechaNac.getFullYear();
 
-    if(mesActual < mesNac){
-        edad--;
+    if (mesActual < mesNac) {
+      edad--;
     }
-    if((mesNac == mesActual) && (diaActual < diaNac)){
-        edad--;
+    if (mesNac == mesActual && diaActual < diaNac) {
+      edad--;
     }
 
     return edad;
+  } // fin del metodo de calculo de edad
 
-  }// fin del metodo de calculo de edad
-
-  esMenor(){
+  esMenor() {
     return this.edad >= 0 && this.edad < 15;
   }
 
-  redireccionar(){
-    this._router.navigate(['/expedientes/']);
+  redireccionar() {
+    this._router.navigate(["/expedientes/"]);
   }
 
+  //Para activar la ayuda del formulario
+  activarPopovers(p1, p2, p3) {
+    this.ayuda = !this.ayuda;
+
+    if (this.ayuda == false) {
+      p1.close();
+      p2.close();
+      p3.close();
+    }
+  }
+  //Para activar un popover X cuando el campo tenga el foco
+  popFocus(px, pxAnterior?) {
+    px.open();
+    if (pxAnterior) {
+      pxAnterior.close();
+    }
+  }
 }
