524e2325376960a5113912af6a794ff048c9b4b3
Alex Carcamo <walter_alx@hotmail.com>
2020-08-21 19:16:05 -0600

    Agregando estilos dataTables

---
+++ src/app/components/consultaComponents/datos-consulta/datos-consulta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datos-consulta',
+  templateUrl: './datos-consulta.component.html',
+  styleUrls: ['./datos-consulta.component.css']
+})
+export class DatosConsultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/consultaComponents/estudios-clinicos/estudios-clinicos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estudios-clinicos',
+  templateUrl: './estudios-clinicos.component.html',
+  styleUrls: ['./estudios-clinicos.component.css']
+})
+export class EstudiosClinicosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
