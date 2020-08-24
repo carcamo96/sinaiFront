2c856e5ffbbf671105e80f10a400d88d4e419cc0
Alex Carcamo <walter_alx@hotmail.com>
2020-08-24 15:05:22 -0600

    Obtiendo datos de receta pre adjuntando

--- src/app/components/consultaComponents/receta/receta.component.ts
+++ src/app/components/consultaComponents/receta/receta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Receta } from '../../../models/receta';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-receta',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecetaComponent implements OnInit {
 
+  receta = {
+    medicamento: '',
+    presentacion: 'Tableta',
+    concentracion: '',
+    medida: 'mg',
+    cantidad: 1,
+    frecuencia: 2,
+    tiempo: 'Hora/s',
+    duracion: 7,
+    lapso: 'Dia/s',
+  }
+
+  recetas: Receta[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log('Arrary: ', this.recetas);
+  }
+
+  onSubmit(f:NgForm){
+
+      console.log("Receta: " ,this.receta);
+      let recetaFull = new Receta(
+          this.receta.medicamento,
+          this.receta.presentacion,
+          this.receta.concentracion + ' ' + this.receta.medida,
+          this.receta.cantidad,
+          'Cada ' + this.receta.frecuencia + ' ' + this.receta.tiempo,
+          'Durante '+ this.receta.duracion + ' ' + this.receta.lapso
+      );
+
+      this.recetas.push(recetaFull);
   }
 
 }
