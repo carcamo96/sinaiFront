322e0432766ec0561a6d82f61688e6f970fb0b77
Alex Carcamo <walter_alx@hotmail.com>
2020-09-14 11:17:11 -0600

    Revisando que todo funcione en la plantilla

--- src/app/components/consultaComponents/estudios-clinicos/estudios-clinicos.component.ts
+++ src/app/components/consultaComponents/estudios-clinicos/estudios-clinicos.component.ts
@@ -7,6 +7,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstudiosClinicosComponent implements OnInit {
 
+  //Arreglo de objetos del catálogo de estudios clinicos temporal, porque los estudios es un mantenimiento a parte
+  estudiosClinicos = [
+    {
+      nombre: "HEMATOLOGÍA",
+      estudios : [
+        "Hemograma", 
+        "Ht - Hb", 
+        "Leucograma",
+        "Plaquetas",
+        "Hemograma - Plaquetas",
+        "Eritrosedimentación",
+        "Frotis de sangre periférica",
+        "Reticulositos",
+        "Células Falsiformes",
+        "Células LE",
+        "Concentrado de Strout (T. Cruzi)"
+      ]
+    },
+    {
+      nombre: "ELECTROLITOS",
+      estudios : [
+        "Sodio", 
+        "Potasio", 
+        "Cloro",
+        "Calcio",
+        "Fósforo",
+        "Magnesio"
+      ]
+    },
+    {
+      nombre: "BACTEREOLOGÍA",
+      estudios : [
+        "Urocultivo", 
+        "Coprocultivo", 
+        "Cultivo de Secreciones:__",
+        "Hemocultivo",
+        "Cultivo Faringeo",
+        "Cultivo Nasal",
+        "Cultivo de Oído:__",
+        "Cultivo de Oído:__",
+        "KDH",
+        "Cultivo de BAAR",
+        "Directo de Secreción",
+        "Coloración de Gram"
+      ]
+    },
+    {
+      nombre: "ENDOCRINOLOGÍA",
+      estudios : [
+        "T3 Total", 
+        "T4 Total", 
+        "TSH",
+        "T3 Libre",
+        "T4 Libre",
+        "TSH Ultrasensible (3ra. Gen)",
+        "Insulina",
+        "Insulina Post Pandrial",
+        "Testosterona",
+        "Prolactina",
+        "FSH",
+        "LH",
+        "Gonad Coriónica hcg - cuartil",
+        "Hormona paratorides",
+        "Hormona del Crecimiento",
+        "Cortisol_am_pm",
+        "Progesterona"
+      ]
+    }
+
+  ];
+
+  estudiosSeleccionados = [];
+
   constructor() { }
 
   ngOnInit(): void {
