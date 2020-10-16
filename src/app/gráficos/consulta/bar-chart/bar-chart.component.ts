a45d6c319652759c9d39364e6865b9164e6d53e1
denn95 <deniron13@gmail.com>
2020-10-16 14:28:25 -0600

    campo dosis

---
+++ src/app/gráficos/consulta/bar-chart/bar-chart.component.ts
@@ -0,0 +1,52 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { NgxSmartModalService } from 'ngx-smart-modal';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styleUrls: ['./bar-chart.component.css']
+})
+export class BarChartComponent implements OnInit, AfterViewInit {
+
+  barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  barChartLabels: Label[] = ['Apple', 'Banana', 'Kiwifruit', 'Blueberry', 'Orange', 'Grapes'];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartPlugins = [];
+
+  barChartData: ChartDataSets[] = [
+    { data: [45, 37, 60, 70, 46, 33], label: 'Best Fruits' }
+  ];
+
+  barChartColors: Color[] = [
+    {
+      backgroundColor: ["#0459b4", "#04b58d", "#04b407", "#b40485", "#b4042d", "#b48b04"]
+    },
+  ];
+
+  constructor(
+    public ngxSmartModalService: NgxSmartModalService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    
+    const obj: Object = {
+      prop1: 'test',
+      prop2: true,
+      prop3: [{a: 'a', b: 'b'}, {c: 'c', d: 'd'}],
+      prop4: 327652175423
+    };
+
+    this.ngxSmartModalService.setModalData(obj, 'myModal');
+  }
+
+}
